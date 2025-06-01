9b7c822aca4f804199daf7dc925f6c3604c08dbd
Alla Vinogradova <79411934+AllaVin@users.noreply.github.com>
2025-06-02 00:07:27 +0200

    Delete version_checking.py
    Wrongly added to the root

--- version_checking.py
+++
@@ -1,7 +0,0 @@
-# import sqlalchemy
-# sqlal_version = sqlalchemy.__version__
-# print(f'SQLAlchemy version: {sqlal_version}')
-# print('----------------------------')
-# from flask import current_app
-# print(current_app.config['SQLALCHEMY_DATABASE_URI'])
-# print('----------------------------')
