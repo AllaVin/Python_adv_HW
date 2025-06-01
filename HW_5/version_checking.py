48d6e6e684d6463e6012c77d0709e9866d2ac424
matyashalla@gmail.com <matyashalla@gmail.com>
2025-06-02 00:39:59 +0200

    Adding HW_5

---
+++ HW_5/version_checking.py
@@ -0,0 +1,7 @@
+# import sqlalchemy
+# sqlal_version = sqlalchemy.__version__
+# print(f'SQLAlchemy version: {sqlal_version}')
+# print('----------------------------')
+# from flask import current_app
+# print(current_app.config['SQLALCHEMY_DATABASE_URI'])
+# print('----------------------------')
