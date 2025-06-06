8834f932abdcff69de4b5a2aaf77168553702fdf
Alla Vinogradova <79411934+AllaVin@users.noreply.github.com>
2025-06-06 12:59:31 +0200

    Delete app directory
    wrongly added

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy() 
-
-from app.models.response import *
-from app.models.question import *
--- app/schemas/questions.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class QuestionCreate(BaseModel):
-    """<UNK> <UNK> <UNK> <UNK>."""
-    text: str = Field()
